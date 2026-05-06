008a3c100338126a431f29e7dc304e3c8e88fa74
Sanhavat <bookbookboo123@gmail.com>
2026-05-06 16:53:04 +0700

    rider section link mock data

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,45 @@ import CheckoutPage from "./pages/cashier/CheckoutPage";
 import TableMap from "./pages/shared/TableMap";
 import OrderList from "./pages/cashier/OrderList";
 import OrderHistory from "./pages/cashier/OrderHistory";
+import DriverDashboard from "./component/rider/DriverDashboard";
+import OrderDetail from "./component/rider/OrderDetail";
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <Navbarmenu />
-      <Routes>
-        <Route path="/" element={<Buttonmenu />} />
-        {/* คุณสามารถเพิ่ม Route อื่นๆ เช่น /menu หรือ /order ได้ที่นี่ */}
-        <Route path="/order" element={<Order />} />
-        <Route path="/home" element={<Index />} />
-
-        {/* เพิ่ม Route ของ chasier / shared (with waiter)  */}
-        <Route path="/cashier/checkout" element={<CheckoutPage />} />
-        <Route path="/shared/tables" element={<TableMap />} />
-        <Route path="/cashier/orders" element={<OrderList />} />
-        <Route path="/cashier/history" element={<OrderHistory />} />
-        <Route path="/menu" element={<Buttonmenu />} />
-        <Route path="/cookBoard" element={<CookBoard />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-      </Routes>
+      <div className="bg-gray-100 min-h-screen">
+        <Routes>
+          <Route path="/" element={<DriverDashboard />} />
+          <Route path="/driver" element={<DriverDashboard />} />
+          <Route path="/driver/order/:orderId" element={<OrderDetail />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
+
+export default App;
+
+// export default function App() {
+//   return (
+//     <Router>
+//       <Navbarmenu />
+//       <Routes>
+//         <Route path="/" element={<Buttonmenu />} />
+//         {/* คุณสามารถเพิ่ม Route อื่นๆ เช่น /menu หรือ /order ได้ที่นี่ */}
+//         <Route path="/order" element={<Order />} />
+//         <Route path="/home" element={<Index />} />
+
+//         {/* เพิ่ม Route ของ chasier / shared (with waiter)  */}
+//         <Route path="/cashier/checkout" element={<CheckoutPage />} />
+//         <Route path="/shared/tables" element={<TableMap />} />
+//         <Route path="/cashier/orders" element={<OrderList />} />
+//         <Route path="/cashier/history" element={<OrderHistory />} />
+//         <Route path="/menu" element={<Buttonmenu />} />
+//         <Route path="/cookBoard" element={<CookBoard />}></Route>
+//         <Route path="/login" element={<Login />}></Route>
+//         <Route path="/register" element={<Register />}></Route>
+//       </Routes>
+//     </Router>
+//   );
+// }
