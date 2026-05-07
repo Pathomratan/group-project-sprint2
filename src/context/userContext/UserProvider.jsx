ce619e3b9bdd79433c7f99ced7a2e12a4bad9ba5
pipatpongzosp-lgtm <pipatpongzosp@gmail.com>
2026-05-07 18:38:27 +0700

    resolve

--- src/context/userContext/UserProvider.jsx
+++ src/context/userContext/UserProvider.jsx
@@ -3,8 +3,9 @@ import { useState,useEffect } from "react";
 
 export const UserProvider=({children})=>{
 const [myUserInfo,setMyUserInfo]=useState(JSON.parse( localStorage.getItem('userInfo')));
-
+// const [myUserInfo,setMyUserInfo]=useState();
 useEffect(()=>{
+    console.log(localStorage.getItem('uerInfo'))
 localStorage.setItem('userInfo',JSON.stringify(myUserInfo));
 },[myUserInfo])
 return(
